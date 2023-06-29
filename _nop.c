51c637c3579a0d08a8cac93ea746f047ca56edac
Ebuube <onwutaebubegideon1555@gmail.com>
2023-06-29 01:46:18 +0000

    Created files for monty

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn't do anything
+ * @stack: address of the pointer to the stack
+ * @line_number: line number being executed
+ *
+ * Return: nothing
+ */
+void _nop(UNUSED stack_t **stack, UNUSED unsigned int line_number)
+{
+}
